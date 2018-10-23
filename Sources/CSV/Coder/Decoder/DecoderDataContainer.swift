d213a77d1315e59dbb2ca2ba169fe960fba717b7
Milan Vit <milanvit@milanvit.net>
2018-10-23 11:06:35 +0900

    Fixed crash on empty columns; improved formatting

--- Sources/CSV/Coder/Decoder/DecoderDataContainer.swift
+++ Sources/CSV/Coder/Decoder/DecoderDataContainer.swift
@@ -4,102 +4,118 @@ import Bits
 final class DecoderDataContainer {
     var allKeys: [CodingKey]?
     let data: [UInt8]
-    
+
     private(set) var row: [String: Bytes]!
     private(set) var cell: Bytes?
     private(set) var header: [String]
-    
+
     private var dataIndex: Int
-    
-    init(data: [UInt8])throws {
-        self.allKeys = nil
-        self.row = [:]
-        self.cell = nil
-        
+
+    init(data: [UInt8]) throws {
+        allKeys = nil
+        row = [:]
+        cell = nil
+
         self.data = data
-        self.header = []
-        self.dataIndex = data.startIndex
-        
-        try self.configure()
+        header = []
+        dataIndex = data.startIndex
+
+        try configure()
     }
-    
-    private func configure()throws {
-        self.header.reserveCapacity(self.data.lazy.split(separator: .newLine).first?.reduce(0) { $1 == .comma ? $0 + 1 : $0 } ?? 0)
-        
-        var cellStart = self.dataIndex
-        var cellEnd = self.dataIndex
-        var inQuote: Bool = false
-        header: while self.dataIndex < data.endIndex {
-            let byte = data[self.dataIndex]
+
+    private func configure() throws {
+        header.reserveCapacity(
+            data.lazy.split(separator: .newLine).first?.reduce(0) {
+                $1 == .comma ? $0 + 1 : $0
+            } ?? 0
+        )
+
+        var cellStart = dataIndex
+        var cellEnd = dataIndex
+        var inQuote = false
+
+        header: while dataIndex < data.endIndex {
+            let byte = data[dataIndex]
+
             switch byte {
             case .quote:
                 inQuote = !inQuote
                 cellEnd += 1
             case .comma:
                 if inQuote { fallthrough }
-                var cell = Array(self.data[cellStart...cellEnd-1])
+                var cell = Array(data[cellStart...cellEnd-1])
                 cell.removeAll { $0 == .quote }
-                try self.header.append(String(cell))
-                
-                cellStart = self.dataIndex + 1
-                cellEnd = self.dataIndex + 1
+                try header.append(String(cell))
+
+                cellStart = dataIndex + 1
+                cellEnd = dataIndex + 1
             case .newLine, .carriageReturn:
                 if inQuote { fallthrough }
-                var cell = Array(self.data[cellStart...cellEnd-1])
+                var cell = Array(data[cellStart...cellEnd-1])
                 cell.removeAll { $0 == .quote }
-                try self.header.append(String(cell))
-                
-                self.dataIndex = byte == .newLine ? self.dataIndex + 1 : self.dataIndex + 2
+                try header.append(String(cell))
+
+                dataIndex = byte == .newLine ? dataIndex + 1 : dataIndex + 2
                 break header
-            default: cellEnd += 1
+            default:
+                cellEnd += 1
             }
-            self.dataIndex += 1
+
+            dataIndex += 1
         }
-        
-        self.row.reserveCapacity(self.header.count)
+
+        row.reserveCapacity(header.count)
     }
-    
+
     func cell(for key: CodingKey) {
-        self.cell = row[key.stringValue]
+        cell = row[key.stringValue]
     }
-    
-    func incremetRow() {
-        guard self.dataIndex < data.endIndex else {
-            self.row = nil
+
+    func incrementRow() {
+        guard dataIndex < data.endIndex else {
+            row = nil
             return
         }
-        
-        var cellStart = self.dataIndex
-        var cellEnd = self.dataIndex
-        var inQuote: Bool = false
-        var columnIndex: Int = 0
-        
-        while self.dataIndex < data.endIndex {
-            let byte = data[self.dataIndex]
+
+        var cellStart = dataIndex
+        var cellEnd = dataIndex
+        var inQuote = false
+        var columnIndex = 0
+
+        while dataIndex < data.endIndex {
+            let byte = data[dataIndex]
             switch byte {
             case .quote:
                 inQuote = !inQuote
                 cellEnd += 1
             case .comma:
                 if inQuote { fallthrough }
-                var cell = Array(self.data[cellStart...cellEnd-1])
+
+                // Empty column
+                guard cellEnd - cellStart >= 1 else {
+                    break
+                }
+
+                var cell = Array(data[cellStart...cellEnd-1])
                 cell.removeAll { $0 == .quote }
-                self.row[header[columnIndex]] = cell
-                
-                cellStart = self.dataIndex + 1
-                cellEnd = self.dataIndex + 1
+                row[header[columnIndex]] = cell
+
+                cellStart = dataIndex + 1
+                cellEnd = dataIndex + 1
                 columnIndex += 1
             case .newLine, .carriageReturn:
                 if inQuote { fallthrough }
-                var cell = Array(self.data[cellStart...cellEnd-1])
+                var cell = Array(data[cellStart...cellEnd-1])
                 cell.removeAll { $0 == .quote }
-                self.row[header[columnIndex]] = cell
-                
-                self.dataIndex = byte == .newLine ? self.dataIndex + 1 : self.dataIndex + 2
+                row[header[columnIndex]] = cell
+
+                dataIndex = byte == .newLine ? dataIndex + 1 : dataIndex + 2
                 return
-            default: cellEnd += 1
+            default:
+                cellEnd += 1
             }
-            self.dataIndex += 1
+
+            dataIndex += 1
         }
     }
 }
--- Sources/CSV/Coder/Decoder/_CSVUnKeyedDecoder.swift
+++ Sources/CSV/Coder/Decoder/_CSVUnKeyedDecoder.swift
@@ -7,41 +7,41 @@ final class _CSVUnkeyedDecoder: UnkeyedDecodingContainer {
     var currentIndex: Int
 
     let decoder: _CSVDecoder
-    
+
     init(decoder: _CSVDecoder, path: CodingPath = []) {
         self.codingPath = path
         self.count = nil
         self.currentIndex = 0
         self.decoder = decoder
-        self.decoder.container.incremetRow()
+        self.decoder.container.incrementRow()
     }
-    
+
     var isAtEnd: Bool {
         return self.decoder.container.row == nil
     }
-    
+
     func error<T>(for type: T.Type) -> Error {
         return DecodingError.typeMismatch(type, DecodingError.Context(codingPath: self.codingPath, debugDescription: "Cannot get \(T.self) value from string array"))
     }
-    
+
     func decodeNil() throws -> Bool {
         return false
     }
-    
+
     func decode(_ type: Bool.Type) throws -> Bool { throw self.error(for: type) }
     func decode(_ type: String.Type) throws -> String { throw self.error(for: type) }
     func decode(_ type: Double.Type) throws -> Double { throw self.error(for: type) }
     func decode(_ type: Float.Type) throws -> Float { throw self.error(for: type) }
     func decode(_ type: Int.Type) throws -> Int { throw self.error(for: type) }
-    
+
     func decode<T>(_ type: T.Type) throws -> T where T : Decodable {
         defer {
             self.currentIndex += 1
-            self.decoder.container.incremetRow()
+            self.decoder.container.incrementRow()
         }
         return try T(from: self.decoder)
     }
-    
+
     func nestedContainer<NestedKey>(keyedBy type: NestedKey.Type) throws -> KeyedDecodingContainer<NestedKey> where NestedKey : CodingKey {
         throw DecodingError.valueNotFound(
             KeyedDecodingContainer<NestedKey>.self,
@@ -51,7 +51,7 @@ final class _CSVUnkeyedDecoder: UnkeyedDecodingContainer {
             )
         )
     }
-    
+
     func nestedUnkeyedContainer() throws -> UnkeyedDecodingContainer {
         throw DecodingError.valueNotFound(
             UnkeyedDecodingContainer.self,
@@ -61,7 +61,7 @@ final class _CSVUnkeyedDecoder: UnkeyedDecodingContainer {
             )
         )
     }
-    
+
     func superDecoder() throws -> Decoder {
         throw DecodingError.valueNotFound(Decoder.self, DecodingError.Context(codingPath: self.codingPath, debugDescription: "Cannot create super decoder from CSV Unkeyed Decoder"))
     }
